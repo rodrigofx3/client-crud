3adfdd763594c454fe17fa3737fc35afbc34a533
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-01 19:05:55 -0300

    Refactor project structure and add Client entity with JPA annotations

--- src/main/java/com/example/clientcrud/ClientCrudApplication.java
+++ src/main/java/com/example/clientcrud/ClientCrudApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClientCrudApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/clientcrud/entities/Client.java
+++ src/main/java/com/example/clientcrud/entities/Client.java
@@ -1,4 +1,27 @@
 package com.example.clientcrud.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    private Integer children;
+
 }
