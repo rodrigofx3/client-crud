e786677ad4281fa6347e8f73a16769100273a057
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-01 19:32:38 -0300

    Add pagination and insert functionality to ClientController and ClientService

--- src/main/java/com/example/clientcrud/controllers/ClientController.java
+++ src/main/java/com/example/clientcrud/controllers/ClientController.java
@@ -2,12 +2,12 @@ package com.example.clientcrud.controllers;
 
 import com.example.clientcrud.dto.ClientDTO;
 import com.example.clientcrud.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/clients")
@@ -21,4 +21,17 @@ public class ClientController {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto) {
+        dto = service.insert(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+
 }
--- src/main/java/com/example/clientcrud/services/ClientService.java
+++ src/main/java/com/example/clientcrud/services/ClientService.java
@@ -4,7 +4,10 @@ import com.example.clientcrud.dto.ClientDTO;
 import com.example.clientcrud.entities.Client;
 import com.example.clientcrud.repositories.ClientRepository;
 import com.example.clientcrud.services.exceptions.ResourceNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,4 +21,23 @@ public class ClientService {
         return new ClientDTO(entity);
     }
 
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> list = repository.findAll(pageable);
+        return list.map(ClientDTO::new);
+    }
+
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
 }
