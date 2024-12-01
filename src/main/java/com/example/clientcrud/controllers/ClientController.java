8decca3290a93c99052441192a2564ffb6936fff
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-01 19:23:18 -0300

    Add findById method to ClientController and update SQL import script for tb_client

--- src/main/java/com/example/clientcrud/controllers/ClientController.java
+++ src/main/java/com/example/clientcrud/controllers/ClientController.java
@@ -1,7 +1,11 @@
 package com.example.clientcrud.controllers;
 
+import com.example.clientcrud.dto.ClientDTO;
 import com.example.clientcrud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,10 @@ public class ClientController {
 
     @Autowired
     private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
 }
